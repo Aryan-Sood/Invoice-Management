d98f148993beecf8be630ae3b6e23597ae323418
Aryan Sood <aryansood12@gmail.com>
2024-11-20 23:52:03 +0530

    add the views

--- server/invoices/views.py
+++ server/invoices/views.py
@@ -1,2 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import InvoiceDetails, Invoices
+from .serializers import InvoiceDetailsSerializer, InvoiceSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    
+    queryset = Invoices.objects.all()
+    serializer_class = InvoiceSerializer
+
+    @action(detail=True, methods=['get'])
+    def details(self, request, pk=None):
+        invoice = self.get_object()
+        details = InvoiceDetails.objects.filter(invoice = invoice)
+        serializer = InvoiceDetailsSerializer(details, many = True)
+        return Response(serializer.data)
+
+class InvoiceDetailViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceDetails.objects.all()
+    serializer_class = InvoiceDetailsSerializer
+    
